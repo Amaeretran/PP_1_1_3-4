63e1dd34516f2eab1c4d87a6a4d4921c3d75340e
Amaeretran <107470981+Amaeretran@users.noreply.github.com>
2022-07-29 17:27:33 +0300

    https://github.com/Amaeretran/PP_1_1_3-4.git

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,8 +2,7 @@ package jm.task.core.jdbc;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        System.out.println("Hello, Java");
         // реализуйте алгоритм здесь
     }
 }
