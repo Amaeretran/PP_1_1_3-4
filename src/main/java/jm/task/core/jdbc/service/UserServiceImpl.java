6c725ddbf241dfeef532f557678b2b3f61a3d08b
Amaeretran <107470981+Amaeretran@users.noreply.github.com>
2022-08-16 20:41:07 +0300

    https://github.com/Amaeretran/PP_1_1_3-4.git

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,12 +1,12 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private static final UserDaoJDBCImpl dao = new UserDaoJDBCImpl();
+    private static final UserDaoHibernateImpl dao = new UserDaoHibernateImpl();
     public void createUsersTable() {
         dao.createUsersTable();
     }
