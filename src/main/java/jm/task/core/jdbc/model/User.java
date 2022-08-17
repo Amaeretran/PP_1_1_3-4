3f199a812ae9886fefa537ee89525121af66e30e
Amaeretran <107470981+Amaeretran@users.noreply.github.com>
2022-08-17 20:29:02 +0300

    https://github.com/Amaeretran/PP_1_1_3-4.git

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,9 +1,11 @@
 package jm.task.core.jdbc.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Entity
 @Table
 public class User {
     @Id
